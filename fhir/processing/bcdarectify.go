e9ae10059ed0970a1f1290afb3ec3cc8c4a3f37a
Samuel Littley <samuellittley@google.com>
2022-11-29 07:20:10 -0800

    Branch fhir.RectifyBCDA to a Processor implementation
    PiperOrigin-RevId: 491628916

---
+++ fhir/processing/bcdarectify.go
@@ -0,0 +1,130 @@
+// Copyright 2022 Google LLC
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//      http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+package processing
+
+import (
+	"context"
+	"errors"
+
+	cpb "github.com/google/fhir/go/proto/google/fhir/proto/r4/core/codes_go_proto"
+	dpb "github.com/google/fhir/go/proto/google/fhir/proto/r4/core/datatypes_go_proto"
+)
+
+type bcdaRectifyProcessor struct {
+	BaseProcessor
+}
+
+// Assert bcdaRectifyProcessor satisfies the Processor interface.
+var _ Processor = &bcdaRectifyProcessor{}
+
+// NewBCDARectifyProcessor creates a Processor which takes BCDA derived FHIR
+// resources, and attempts to rectify them to fix known issues in source mapping
+// (in the ways described below). This is a temporary, non-ideal, and minimalist
+// approach, which aims to make the FHIR compatible with base R4 expectations so
+// that otherwise useful data can be easily uploaded to FHIR store with
+// validation for other areas still intact.
+func NewBCDARectifyProcessor() Processor {
+	return &bcdaRectifyProcessor{}
+}
+
+func (brp *bcdaRectifyProcessor) Process(ctx context.Context, resource ResourceWrapper) error {
+	switch resource.Type() {
+	case cpb.ResourceTypeCode_COVERAGE:
+		return brp.rectifyCoverage(ctx, resource)
+	case cpb.ResourceTypeCode_EXPLANATION_OF_BENEFIT:
+		return brp.rectifyExplanationOfBenefit(ctx, resource)
+	default:
+		return brp.Output(ctx, resource)
+	}
+}
+
+func (brp *bcdaRectifyProcessor) rectifyExplanationOfBenefit(ctx context.Context, resource ResourceWrapper) error {
+	proto, err := resource.Proto()
+	if err != nil {
+		return err
+	}
+	e := proto.GetExplanationOfBenefit()
+	if e == nil {
+		return errors.New("resource was not ExplanationOfBenefit")
+	}
+
+	// BCDA ExplanationOfBenefits don't have provider references mapped, which is
+	// required by base FHIR R4. So, we put in an extension explaining that this
+	// field is unmapped by the source data if we see the Provider isn't set at
+	// all. If the message exists at all, we don't override it at this time.
+	if e.Provider == nil {
+		e.Provider = &dpb.Reference{
+			Extension: []*dpb.Extension{getUnmappedExtension()},
+		}
+	}
+
+	// Focal is also unset by BCDA, so we mark it with an extension too if not
+	// present.
+	for _, i := range e.GetInsurance() {
+		if i.Focal == nil {
+			i.Focal = &dpb.Boolean{Extension: []*dpb.Extension{getUnmappedExtension()}}
+		}
+	}
+
+	// Sometimes productOrService is unset in ExplanationOfBenefit.item[x] (but is
+	// required by FHIR) so we mark it with the extension if not present.
+	for _, i := range e.GetItem() {
+		if i.ProductOrService == nil {
+			i.ProductOrService = &dpb.CodeableConcept{Extension: []*dpb.Extension{getUnmappedExtension()}}
+		}
+	}
+
+	return brp.Output(ctx, resource)
+}
+
+func (brp *bcdaRectifyProcessor) rectifyCoverage(ctx context.Context, resource ResourceWrapper) error {
+	proto, err := resource.Proto()
+	if err != nil {
+		return err
+	}
+	cov := proto.GetCoverage()
+	if cov == nil {
+		return errors.New("resource was not ExplanationOfBenefit")
+	}
+	// BCDA Coverage resources have invalid Coverage.contract references that
+	// appear to be placeholders (they reference other Coverages instead of other
+	// Contracts, which leads to a validation failure). We look for a specific
+	// reference that we expect to be a placeholder, and if it exists, replace it
+	// with a similar placeholder (excpet that it's a Contract/ reference instead
+	// of a Coverage/ reference). More details at b/175394994#comment24.
+	for _, contract := range cov.GetContract() {
+		if contract.GetCoverageId().GetValue() == "part-a-contract1" {
+			// We only try to correct this placeholder reference for safety. We
+			// replace the Coverage reference with a Contract reference with the same
+			// value.
+			contract.Reference = &dpb.Reference_ContractId{&dpb.ReferenceId{Value: "part-a-contract1"}}
+		}
+	}
+
+	return brp.Output(ctx, resource)
+}
+
+func getUnmappedExtension() *dpb.Extension {
+	return &dpb.Extension{
+		// TODO(b/175394994): figure out the right extension URI we want to use
+		// before release, and if we want to create an extension profile for it.
+		Url: &dpb.Uri{Value: "https://g.co/unmapped-by-bcda"},
+		Value: &dpb.Extension_ValueX{
+			Choice: &dpb.Extension_ValueX_StringValue{
+				StringValue: &dpb.String{Value: "This is a required FHIR R4 Field, but not mapped by BCDA, which is why we expect it to be empty."},
+			},
+		},
+	}
+}
---
+++ fhir/processing/bcdarectify_test.go
@@ -0,0 +1,124 @@
+// Copyright 2022 Google LLC
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//      http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+package processing_test
+
+import (
+	"context"
+	"testing"
+
+	"github.com/google/go-cmp/cmp"
+	"github.com/google/medical_claims_tools/fhir/processing"
+	"github.com/google/medical_claims_tools/internal/testhelpers"
+
+	cpb "github.com/google/fhir/go/proto/google/fhir/proto/r4/core/codes_go_proto"
+)
+
+func TestBcdaRectifyProcessor(t *testing.T) {
+	cases := []struct {
+		name         string
+		resourceType cpb.ResourceTypeCode_Value
+		jsonIn       []byte
+		wantJSON     []byte
+	}{
+		{
+			name:         "ExplanationOfBenefitWithoutProviderAndFocal",
+			resourceType: cpb.ResourceTypeCode_EXPLANATION_OF_BENEFIT,
+			jsonIn:       []byte(`{"resourceType": "ExplanationOfBenefit", "id": "123", "insurance": [{"coverage": {"reference": "coverage"}}]}`),
+			wantJSON: []byte(`{` +
+				`"id":"123",` +
+				`"provider":{"extension":[{"url":"https://g.co/unmapped-by-bcda","valueString":"This is a required FHIR R4 Field, but not mapped by BCDA, which is why we expect it to be empty."}]},` +
+				`"insurance": [{"_focal": {"extension":[{"url":"https://g.co/unmapped-by-bcda","valueString":"This is a required FHIR R4 Field, but not mapped by BCDA, which is why we expect it to be empty."}]},"coverage": {"reference": "coverage"},"focal": false}],` +
+				`"resourceType":"ExplanationOfBenefit"}`),
+		},
+		{
+			name:         "ExplanationOfBenefitWithoutProvider",
+			resourceType: cpb.ResourceTypeCode_EXPLANATION_OF_BENEFIT,
+			jsonIn:       []byte(`{"resourceType": "ExplanationOfBenefit", "id": "123", "insurance": [{"coverage": {"reference": "coverage"}, "focal": false}]}`),
+			wantJSON: []byte(`{` +
+				`"id":"123",` +
+				`"provider":{"extension":[{"url":"https://g.co/unmapped-by-bcda","valueString":"This is a required FHIR R4 Field, but not mapped by BCDA, which is why we expect it to be empty."}]},` +
+				`"insurance": [{"coverage": {"reference": "coverage"},"focal": false}],` +
+				`"resourceType":"ExplanationOfBenefit"}`),
+		},
+		{
+			name:         "ExplanationOfBenefitWithoutFocal",
+			resourceType: cpb.ResourceTypeCode_EXPLANATION_OF_BENEFIT,
+			jsonIn:       []byte(`{"resourceType": "ExplanationOfBenefit", "id": "123", "provider": {"reference": "provider"}, "insurance": [{"coverage": {"reference": "coverage"}}]}`),
+			wantJSON: []byte(`{` +
+				`"id":"123",` +
+				`"provider": {"reference": "provider"},` +
+				`"insurance": [{"_focal": {"extension":[{"url":"https://g.co/unmapped-by-bcda","valueString":"This is a required FHIR R4 Field, but not mapped by BCDA, which is why we expect it to be empty."}]},"coverage": {"reference": "coverage"},"focal": false}],` +
+				`"resourceType":"ExplanationOfBenefit"}`),
+		},
+		{
+			name:         "ExplanationOfBenefitFullyPopulated",
+			resourceType: cpb.ResourceTypeCode_EXPLANATION_OF_BENEFIT,
+			jsonIn:       []byte(`{"resourceType": "ExplanationOfBenefit", "id": "123", "provider":{"reference": "123"}, "insurance": [{"coverage": {"reference": "coverage"}, "focal": false}], "item": [{"sequence": 1, "productOrService": {"text": "test"}}]}`),
+			wantJSON:     []byte(`{"resourceType": "ExplanationOfBenefit", "id": "123", "provider":{"reference": "123"}, "insurance": [{"coverage": {"reference": "coverage"}, "focal": false}], "item": [{"sequence": 1, "productOrService": {"text": "test"}}]}`),
+		},
+		{
+			name:         "ExplanationOfBenefitWithItemsMissingProductOrService",
+			resourceType: cpb.ResourceTypeCode_EXPLANATION_OF_BENEFIT,
+			jsonIn:       []byte(`{"resourceType": "ExplanationOfBenefit", "id": "123", "provider":{"reference": "123"}, "insurance": [{"coverage": {"reference": "coverage"}, "focal": false}], "item": [{"sequence": 1}]}`),
+			wantJSON:     []byte(`{"resourceType": "ExplanationOfBenefit", "id": "123", "provider":{"reference": "123"}, "insurance": [{"coverage": {"reference": "coverage"}, "focal": false}], "item": [{"sequence": 1, "productOrService": {"extension":[{"url":"https://g.co/unmapped-by-bcda","valueString":"This is a required FHIR R4 Field, but not mapped by BCDA, which is why we expect it to be empty."}]}}]}`),
+		},
+		{
+			name:         "Patient",
+			resourceType: cpb.ResourceTypeCode_PATIENT,
+			jsonIn:       []byte(`{"resourceType": "Patient", "id": "123"}`),
+			wantJSON:     []byte(`{"resourceType": "Patient", "id": "123"}`),
+		},
+		{
+			name:         "SimpleCoverage",
+			resourceType: cpb.ResourceTypeCode_COVERAGE,
+			jsonIn:       []byte(`{"resourceType": "Coverage", "id": "123"}`),
+			wantJSON:     []byte(`{"resourceType": "Coverage", "id": "123"}`),
+		},
+		{
+			name:         "CoverageWithReplacableContract",
+			resourceType: cpb.ResourceTypeCode_COVERAGE,
+			jsonIn:       []byte(`{"resourceType": "Coverage", "id": "123", "contract": [{"reference": "Coverage/part-a-contract1"}]}`),
+			wantJSON:     []byte(`{"resourceType": "Coverage", "id": "123", "contract": [{"reference": "Contract/part-a-contract1"}]}`),
+		},
+		{
+			name:         "CoverageWithUnreplacableContract",
+			resourceType: cpb.ResourceTypeCode_COVERAGE,
+			jsonIn:       []byte(`{"resourceType": "Coverage", "id": "123", "contract": [{"reference": "Coverage/something-else"}]}`),
+			wantJSON:     []byte(`{"resourceType": "Coverage", "id": "123", "contract": [{"reference": "Coverage/something-else"}]}`),
+		},
+	}
+
+	for _, tc := range cases {
+		t.Run(tc.name, func(t *testing.T) {
+			ts := &testSink{}
+			p, err := processing.NewPipeline([]processing.Processor{processing.NewBCDARectifyProcessor()}, []processing.Sink{ts})
+			if err != nil {
+				t.Fatalf("NewPipeline() returned unexpected error: %v", err)
+			}
+			if err := p.Process(context.Background(), tc.resourceType, "", tc.jsonIn); err != nil {
+				t.Fatalf("pipeline.Process(..., %s) returned unexpected error: %v", tc.jsonIn, err)
+			}
+			gotJSON, err := ts.writtenResources[0].JSON()
+			if err != nil {
+				t.Fatalf("writtenResource.JSON() returned unexpected error: %v", err)
+			}
+			normalizedWantJSON := testhelpers.NormalizeJSON(t, tc.wantJSON)
+			normalizedGotJSON := testhelpers.NormalizeJSON(t, gotJSON)
+			if !cmp.Equal(normalizedGotJSON, normalizedWantJSON) {
+				t.Errorf("pipeline.Process(..., %s) produced unexpected output. got: %s, want: %s", tc.jsonIn, normalizedGotJSON, normalizedWantJSON)
+			}
+		})
+	}
+}
