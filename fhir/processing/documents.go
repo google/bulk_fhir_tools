c4f04e2b2e956a1f717e9ec346711af71ebeb743
Samuel Littley <samuellittley@google.com>
2022-12-21 05:08:42 -0800

    Add support for downloading documents referenced in DocumentReference resources
    PiperOrigin-RevId: 496895282

---
+++ fhir/processing/documents.go
@@ -0,0 +1,212 @@
+// Copyright 2022 Google LLC
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//      http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+package processing
+
+import (
+	"bytes"
+	"context"
+	"errors"
+	"fmt"
+	"hash/crc32"
+	"mime"
+	"net/http"
+	"os"
+	"path/filepath"
+
+	log "github.com/golang/glog"
+	"cloud.google.com/go/storage"
+	"github.com/google/medical_claims_tools/bulkfhir"
+	"github.com/google/medical_claims_tools/gcs"
+	"github.com/google/medical_claims_tools/internal/counter"
+
+	cpb "github.com/google/fhir/go/proto/google/fhir/proto/r4/core/codes_go_proto"
+	dpb "github.com/google/fhir/go/proto/google/fhir/proto/r4/core/datatypes_go_proto"
+	drpb "github.com/google/fhir/go/proto/google/fhir/proto/r4/core/resources/document_reference_go_proto"
+)
+
+type fileWriter interface {
+	writeFile(ctx context.Context, filename string, data []byte) (string, error)
+}
+
+type gcsFileWriter struct {
+	client            gcs.Client
+	bucket, directory string
+}
+
+func (gfw *gcsFileWriter) writeFile(ctx context.Context, filename string, data []byte) (string, error) {
+	gcsPath := filepath.Join(gfw.directory, filename)
+	gcsObj := gfw.client.Bucket(gfw.bucket).Object(gcsPath)
+	attrs, err := gcsObj.Attrs(ctx)
+	needsUpload := true
+	if err != nil {
+		if !errors.Is(err, storage.ErrObjectNotExist) {
+			return "", err
+		}
+	} else {
+		// The file exists, so check if the CRC32C checksum of the file matches. If
+		// so, we treat the file as unchanged and don't upload it again.
+		crc32c := crc32.Checksum(data, crc32.MakeTable(crc32.Castagnoli))
+		if attrs.CRC32C == crc32c {
+			needsUpload = false
+		}
+	}
+	if needsUpload {
+		w := gcsObj.NewWriter(ctx)
+		if _, err := w.Write(data); err != nil {
+			return "", err
+		}
+		if err := w.Close(); err != nil {
+			return "", err
+		}
+	}
+	return fmt.Sprintf("gs://%s/%s", gfw.bucket, gcsPath), nil
+}
+
+type localFileWriter struct {
+	directory string
+}
+
+func (lfw *localFileWriter) writeFile(ctx context.Context, filename string, data []byte) (string, error) {
+	fullPath := filepath.Join(lfw.directory, filename)
+	return fmt.Sprintf("file://%s", fullPath), os.WriteFile(fullPath, data, 0666)
+}
+
+type documentsProcessor struct {
+	BaseProcessor
+	authenticator         bulkfhir.Authenticator
+	httpClient            *http.Client
+	fileWriter            fileWriter
+	retrievalErrorCounter *counter.Counter
+}
+
+var _ Processor = &documentsProcessor{}
+
+// DocumentsProcessorConfig contains the configuration needed for creating a
+// Documents Processor.
+type DocumentsProcessorConfig struct {
+	// Some APIs may require different authentication to be used for retrieving
+	// documents vs the authentication used for the Bulk FHIR export process.
+	Authenticator                        bulkfhir.Authenticator
+	HTTPClient                           *http.Client
+	LocalDirectory                       string
+	GCSEndpoint, GCSBucket, GCSDirectory string
+}
+
+// NewDocumentsProcessor creates a Processor which downloads documents from
+// the URLs found in DocumentReference resources, and replaces those URLs with
+// URIs for the downloaded files.
+func NewDocumentsProcessor(ctx context.Context, cfg *DocumentsProcessorConfig) (Processor, error) {
+	var fw fileWriter
+	if cfg.LocalDirectory != "" {
+		fw = &localFileWriter{cfg.LocalDirectory}
+	} else {
+		gcsClient, err := gcs.NewClient(ctx, cfg.GCSBucket, cfg.GCSEndpoint)
+		if err != nil {
+			return nil, err
+		}
+		fw = &gcsFileWriter{
+			client:    gcsClient,
+			bucket:    cfg.GCSBucket,
+			directory: cfg.GCSDirectory,
+		}
+	}
+	return &documentsProcessor{
+		authenticator:         cfg.Authenticator,
+		httpClient:            cfg.HTTPClient,
+		fileWriter:            fw,
+		retrievalErrorCounter: counter.New(),
+	}, nil
+}
+
+func (dp *documentsProcessor) Process(ctx context.Context, resource ResourceWrapper) error {
+	switch resource.Type() {
+	case cpb.ResourceTypeCode_DOCUMENT_REFERENCE:
+		return dp.processDocument(ctx, resource)
+	default:
+		return dp.Output(ctx, resource)
+	}
+}
+
+func (dp *documentsProcessor) processDocument(ctx context.Context, resource ResourceWrapper) error {
+	proto, err := resource.Proto()
+	if err != nil {
+		return err
+	}
+	dr := proto.GetDocumentReference()
+	if dr == nil {
+		return fmt.Errorf("resource %s was not DocumentReference", dr.GetId().GetValue())
+	}
+
+	for i, c := range dr.GetContent() {
+		if err := dp.downloadFileAndUpdateResource(ctx, dr.GetId().GetValue(), i, c); err != nil {
+			return err
+		}
+	}
+
+	return dp.Output(ctx, resource)
+}
+
+func (dp *documentsProcessor) downloadFileAndUpdateResource(ctx context.Context, documentID string, index int, content *drpb.DocumentReference_Content) error {
+	url := content.GetAttachment().GetUrl().GetValue()
+	if url == "" {
+		return nil
+	}
+
+	req, err := http.NewRequest(http.MethodGet, url, http.NoBody)
+	if err != nil {
+		return err
+	}
+	if err := dp.authenticator.AddAuthenticationToRequest(dp.httpClient, req); err != nil {
+		return err
+	}
+	resp, err := dp.httpClient.Do(req)
+	if err != nil {
+		return err
+	}
+	if resp.StatusCode != http.StatusOK {
+		log.Errorf("request for %s returned unexpected status %s", url, resp.Status)
+		dp.retrievalErrorCounter.Increment()
+		return nil
+	}
+	var buf bytes.Buffer
+	if _, err := buf.ReadFrom(resp.Body); err != nil {
+		return err
+	}
+	if err := resp.Body.Close(); err != nil {
+		return err
+	}
+
+	// Best effort attempt to determine an appropriate file extension; if not
+	// available we just save the file without an extension.
+	var ext string
+	exts, err := mime.ExtensionsByType(content.GetAttachment().GetContentType().GetValue())
+	if err == nil && len(exts) > 0 {
+		ext = exts[0]
+	}
+
+	fileURL, err := dp.fileWriter.writeFile(ctx, fmt.Sprintf("%s_%d%s", documentID, index, ext), buf.Bytes())
+	if err != nil {
+		return err
+	}
+	content.GetAttachment().Url = &dpb.Url{Value: fileURL}
+	return nil
+}
+
+func (dp *documentsProcessor) Finalize(ctx context.Context) error {
+	if retrievalErrors := dp.retrievalErrorCounter.CloseAndGetCount(); retrievalErrors > 0 {
+		log.Errorf("Document processor failed to retrieve %d documents. Check logs for details", retrievalErrors)
+	}
+	return nil
+}
---
+++ fhir/processing/documents_test.go
@@ -0,0 +1,153 @@
+// Copyright 2022 Google LLC
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//      http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+package processing_test
+
+import (
+	"context"
+	"fmt"
+	"net/http"
+	"net/http/httptest"
+	"os"
+	"strings"
+	"testing"
+
+	"github.com/google/go-cmp/cmp"
+	"github.com/google/medical_claims_tools/bulkfhir"
+	"github.com/google/medical_claims_tools/fhir/processing"
+	"github.com/google/medical_claims_tools/internal/testhelpers"
+
+	cpb "github.com/google/fhir/go/proto/google/fhir/proto/r4/core/codes_go_proto"
+)
+
+func TestDocumentsProcessor(t *testing.T) {
+	ctx := context.Background()
+
+	server := httptest.NewServer(http.HandlerFunc(func(w http.ResponseWriter, req *http.Request) {
+		switch req.URL.Path {
+		case "/auth":
+			w.Write([]byte(`{"access_token": "123", "expires_in": 1200}`))
+		case "/document.pdf":
+			// We don't check file contents, so this doesn't need to be real PDF data.
+			w.Write([]byte(`pdf data`))
+		case "/document.xml":
+			// We don't check file contents, so this doesn't need to be real XML data.
+			w.Write([]byte(`xml data`))
+		case "/nonexistent.pdf":
+			w.WriteHeader(http.StatusNotFound)
+		default:
+			w.WriteHeader(http.StatusNotFound)
+			w.Write([]byte(fmt.Sprintf("Unrecognised path %s", req.URL.Path)))
+		}
+	}))
+	defer server.Close()
+
+	for _, tc := range []struct {
+		description  string
+		resourceType cpb.ResourceTypeCode_Value
+		input        string
+		wantError    bool
+		wantJSON     string
+	}{
+		{
+			description:  "patient resource ignored",
+			resourceType: cpb.ResourceTypeCode_PATIENT,
+			input:        `{"resourceType": "Patient", "id": "123"}`,
+			wantJSON:     `{"resourceType": "Patient", "id": "123"}`,
+		},
+		{
+			description:  "With documents",
+			resourceType: cpb.ResourceTypeCode_DOCUMENT_REFERENCE,
+			input:        `{"resourceType": "DocumentReference", "id": "documentid", "content": [{"attachment": {"contentType": "application/pdf", "url": "` + server.URL + `/document.pdf"}}, {"attachment": {"contentType": "text/xml", "url": "` + server.URL + `/document.xml"}}]}`,
+			wantJSON:     `{"resourceType": "DocumentReference", "id": "documentid", "content": [{"attachment": {"contentType": "application/pdf", "url": "FILEPATH0"}}, {"attachment": {"contentType": "text/xml", "url": "FILEPATH1"}}]}`,
+		},
+		{
+			description:  "Non-existent document",
+			resourceType: cpb.ResourceTypeCode_DOCUMENT_REFERENCE,
+			input:        `{"resourceType": "DocumentReference", "id": "documentid", "content": [{"attachment": {"contentType": "application/pdf", "url": "` + server.URL + `/nonexistent.pdf"}}]}`,
+			wantJSON:     `{"resourceType": "DocumentReference", "id": "documentid", "content": [{"attachment": {"contentType": "application/pdf", "url": "` + server.URL + `/nonexistent.pdf"}}]}`,
+		},
+		{
+			description:  "Inline data unchanged",
+			resourceType: cpb.ResourceTypeCode_DOCUMENT_REFERENCE,
+			input:        `{"resourceType": "DocumentReference", "id": "documentid", "content": [{"attachment": {"contentType": "text/plain", "data": "SGVsbG8gV29ybGQh"}}]}`,
+			wantJSON:     `{"resourceType": "DocumentReference", "id": "documentid", "content": [{"attachment": {"contentType": "text/plain", "data": "SGVsbG8gV29ybGQh"}}]}`,
+		},
+	} {
+		t.Run(tc.description, func(t *testing.T) {
+			tempdir, err := os.MkdirTemp("", "")
+			if err != nil {
+				t.Fatal(err)
+			}
+			defer os.RemoveAll(tempdir)
+			ts := &testSink{}
+			authenticator, err := bulkfhir.NewHTTPBasicOAuthAuthenticator("username", "password", server.URL+"/auth", nil)
+			if err != nil {
+				t.Fatal(err)
+			}
+			proc, err := processing.NewDocumentsProcessor(ctx, &processing.DocumentsProcessorConfig{
+				Authenticator:  authenticator,
+				HTTPClient:     http.DefaultClient,
+				LocalDirectory: tempdir,
+			})
+			if err != nil {
+				t.Fatalf("NewDocumentProcessor() returned unexpected error: %v", err)
+			}
+			p, err := processing.NewPipeline([]processing.Processor{proc}, []processing.Sink{ts})
+			if err != nil {
+				t.Fatalf("NewPipeline() returned unexpected error: %v", err)
+			}
+			err = p.Process(context.Background(), tc.resourceType, "", []byte(tc.input))
+			if tc.wantError && err == nil {
+				t.Fatalf("pipeline.Process(..., %s) did not return expected error", tc.input)
+			} else if !tc.wantError && err != nil {
+				t.Fatalf("pipeline.Process(..., %s) returned unexpected error: %v", tc.input, err)
+			}
+
+			// We can't guarantee the filename of the output files, because they
+			// depend on the hosts MIME type configuration (we have seen cases where
+			// different host produce unusual filename extensions. Instead, we verify
+			// that the filename in the output resource exists, and then populate the
+			// wanted resource with the filename that was written.
+
+			gotResource, err := ts.writtenResources[0].Proto()
+			if err != nil {
+				t.Fatalf("writtenResource.Proto() returned unexpected error: %v", err)
+			}
+			for i, c := range gotResource.GetDocumentReference().GetContent() {
+				url := c.GetAttachment().GetUrl().GetValue()
+				if strings.HasPrefix(url, "file://") {
+					_, err := os.Stat(strings.TrimPrefix(url, "file://"))
+					if err != nil {
+						t.Errorf("Failed to stat expected file %s: %v", url, err)
+					}
+				}
+				// We're inserting a file path potentially containing backslashes (on
+				// Windows) into an encoded JSON string, so we need to escape any
+				// backslashes.
+				tc.wantJSON = strings.Replace(tc.wantJSON, fmt.Sprintf("FILEPATH%d", i), strings.ReplaceAll(url, `\`, `\\`), 1)
+			}
+
+			gotJSON, err := ts.writtenResources[0].JSON()
+			if err != nil {
+				t.Fatalf("writtenResource.JSON() returned unexpected error: %v", err)
+			}
+			normalizedWantJSON := testhelpers.NormalizeJSON(t, []byte(tc.wantJSON))
+			normalizedGotJSON := testhelpers.NormalizeJSON(t, gotJSON)
+			if !cmp.Equal(normalizedGotJSON, normalizedWantJSON) {
+				t.Errorf("pipeline.Process(..., %s) produced unexpected output. got: %s, want: %s", tc.input, normalizedGotJSON, normalizedWantJSON)
+			}
+		})
+	}
+}
